e29394d7a054a808bcdd24dca3d34834e73709ae
YakaryBovine <kaz9876@gmail.com>
2019-08-26 17:59:28 +1200

    build tools pass

--- build.bat
+++ build.bat
@@ -1,18 +1,23 @@
 @echo off
 echo Starting build...
+md temp
 
 echo Copying base map to output directory...
 copy sourceMaps\BlankMap.w3x compiledMaps\OutputMap.w3x
 
-echo Merging .j files in /jass into one...
-copy NUL compiledMaps\\merged.j
+echo MPQEditor: Extracting war3map.j...
+buildTools\\MPQEditor\MPQEditor.exe extract compiledMaps\\OutputMap.w3x war3map.j compiledMaps
+
+echo Merging project .j files into war3map.j...
 pushd jass\
 for /r %%a in (*.j) do (
-  type "%%a" >> "c:\Users\MC\Documents\YakaryBovine Maps\AzerothWarsLR\compiledMaps\merged.j"
-  echo. >> "c:\Users\MC\Documents\YakaryBovine Maps\AzerothWarsLR\compiledMaps\merged.j"
+  type "%%a" >> "e:\\Users\\Zak\\Documents\\YakaryBovine Maps\\AzerothWarsLR\\compiledMaps\\war3map.j"
+  echo. >> "e:\\Users\\Zak\\Documents\\YakaryBovine Maps\\AzerothWarsLR\\compiledMaps\\war3map.j"
 )
 popd
 
-echo Compiling jass...
-copy NUL compiledMaps\\compiled.j
-buildTools\\JassHelper\\jasshelper.exe buildTools\\common.j buildTools\\Blizzard.j compiledMaps\\merged.j compiledMaps\\compiled.j --scriptonly
+echo JassHelper: Processing war3map.j...
+buildTools\\JassHelper\\jasshelper.exe buildTools\\common.j buildTools\\Blizzard.j compiledMaps\\war3map.j compiledMaps\\war3map.j --scriptonly
+
+echo Cleaning up...
+rd /s /q temp
